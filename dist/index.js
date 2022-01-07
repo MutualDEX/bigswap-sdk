616ad4123d76bb10f33c87c25b3c49adf4efbcfd
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2022-01-07 12:01:55 -0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./golswap-sdk.cjs.production.min.js')
+  module.exports = require('./bigswap-sdk.cjs.production.min.js')
 } else {
-  module.exports = require('./golswap-sdk.cjs.development.js')
+  module.exports = require('./bigswap-sdk.cjs.development.js')
 }
